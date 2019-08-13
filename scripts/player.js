0b43cba685c89891cdda6475b9c730ffad2ee0a4
Tim Moua <49731829+tjmoua19@users.noreply.github.com>
2019-08-13 12:40:42 -0400

    Update player.js

--- scripts/player.js
+++ scripts/player.js
@@ -13,17 +13,14 @@ class Player {
   getTime() {
     return this.soundObject.getTime();
   }
-  
+
   playPause (song = this.currentlyPlaying) {
-     console.log("get duration 1 = " + player.getDurarion());
-    
+    console.log("get duration 1 = " + player.getDuration());
+
     if (this.currentlyPlaying !== song) {
-      // Stop the currently playing sound file (even if nothing is playing)
       this.soundObject.stop();
-      // Clear classes on the song that's currently playing
       this.currentlyPlaying.element.removeClass('playing paused');
-      
-      // Update our currentlyPlaying and playState properties
+
       this.currentlyPlaying = song;
       this.playState = 'stopped';
       this.soundObject = new buzz.sound(this.currentlyPlaying.soundFileUrl);
@@ -43,14 +40,15 @@ class Player {
       this.currentlyPlaying.element.removeClass('playing').addClass('paused');
       console.log("get duration 4 = " + player.getDuration());
     }
-     console.log("get duration 5 = " + player.getDuration());
+    
+    console.log("get duration 5 = " + player.getDuration());
   }
-  
+
   skipTo (percent) {
     if (this.playState !== 'playing') { return }
     this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
   }
-  
+
   setVolume (percent) {
     this.volume = percent;
     this.soundObject.setVolume(percent);
