25387b00de4644c62c7c695d1ed684b54c4a6b43
Tim Moua <49731829+tjmoua19@users.noreply.github.com>
2019-08-15 12:38:06 -0400

    Update player-bar.js

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,41 +1,52 @@
 {
-  $('button#play-pause').on('click', function() {
-    player.playPause();
-    $(this).attr('playState', player.playState);
-    $('button#play-pause').attr('playState', player.playState);
-  });
+   $('button#play-pause').on('click', function() {
+     helper.playPauseAndUpdate();
+     $(this).attr('playState', player.playState);
 
-  $('button#next').on('click', function() {
+     const totalTime = player.prettyTime(player.currentlyPlaying.duration);
+      $('#time-control .total-time').text( totalTime );
+   });
+
+   $('button#next').on('click', function() {
      if (player.playState !== 'playing') { return; }
+
      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
      const nextSongIndex = currentSongIndex + 1;
      if (nextSongIndex >= album.songs.length) { return; }
 
      const nextSong = album.songs[nextSongIndex];
-     player.playPause(nextSong);
+     helper.playPauseAndUpdate(nextSong);
+
+     $('#time-control .total-time').text( player.prettyTime(nextSong.duration) );
    });
 
    $('button#previous').on('click', function() {
-      if (player.playState !== 'playing') { return; }
-      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-      const previousSongIndex = currentSongIndex - 1;
-      if (previousSongIndex >= album.songs.length) { return; }
+     if (player.playState !== 'playing') { return; }
 
-      const previousSong = album.songs[previousSongIndex];
-      player.playPause(previousSong);
+     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+     const nextSongIndex = currentSongIndex - 1;
+     if (nextSongIndex >= album.songs.length) { return; }
+
+     const nextSong = album.songs[nextSongIndex];
+     helper.playPauseAndUpdate(nextSong);
+
+     $('#time-control .total-time').text( player.prettyTime(nextSong.duration) );
+   });
+
+   $('#time-control input').on('input', function (event) {
+     player.skipTo(event.target.value);
     });
 
-   $('#time-control input').on('input', function (event){
-     player.skipTo(event.target.value);
-   });
+    $('#volume-control input').on('input', function (event) {
+      player.setVolume(event.target.value);
+    });
 
    setInterval( () => {
      if (player.playState !== 'playing') { return; }
      const currentTime = player.getTime();
      const duration = player.getDuration();
      const percent = (currentTime / duration) * 100;
-     $('#time-control .current-time').text( currentTime );
+     $('#time-control .current-time').text( player.prettyTime(currentTime) );
      $('#time-control input').val(percent);
-     $('#time-control .total-time').text( duration );
    }, 1000);
-}
+ }
