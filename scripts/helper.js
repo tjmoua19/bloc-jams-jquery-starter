2770a7824c2746a2c4e63fa32edcb28ab4969ed5
Tim Moua <49731829+tjmoua19@users.noreply.github.com>
2019-08-16 11:57:40 -0400

    Update helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,9 @@
-class Helper{
-  playPauseAndUpdate(song){
-    player.playPause(song);
-    $('#time-control .total-time').text(player.prettyTime(player.currentlyPlaying.duration));
-  }
-}
+class Helper {
+    playPauseAndUpdate(song) {
+        player.playPause(song);
+        const duration = player.currentlyPlaying.duration;
+        $('#time-control .total-time').text(player.prettyTime(duration));
+    }    
+ }
 
 const helper = new Helper();
